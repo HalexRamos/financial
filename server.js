b79e8d8bee38d47a9747af6720bf31eacd963460
halexramos <halexramos96@gmail.com>
2022-02-26 15:09:20 -0300

    Sass

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
-    res.render('index', { text: 'Hello World' });
+    res.render('login');
 });
 
 app.listen(3000, () => console.info('Listening on port 3000'));
