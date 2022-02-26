cb962fa6036f69631d93aa169ff8c6150a6821a9
halexramos <halexramos96@gmail.com>
2022-02-26 17:23:33 -0300

    Signup Layout

--- server.js
+++ server.js
@@ -14,4 +14,8 @@ app.get('', (req, res) => {
     res.render('login');
 });
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
 app.listen(3000, () => console.info('Listening on port 3000'));
